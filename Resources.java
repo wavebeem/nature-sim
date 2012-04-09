8c9bf2ed918f535b8d7eb0954872b699632813fd
Brian Mock <mock.brian@gmail.com>
2012-04-09 16:18:29 -0700

    New grid stuff.

---
+++ Resources.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Resources {
+    public static Image gridSquareOverlayImage;
+
+    public static void load() {
+        gridSquareOverlayImage = Util.loadImage("img/overlay.png");
+    }
+}
