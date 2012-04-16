cefdf01143b6e4ca14373f660fedbda2528b46c5
Trevor Griswold <tgriswol@willamette.edu>
2012-04-16 01:24:57 -0700

    Started to implement an act for Fox

--- Fox.java
+++ Fox.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.awt.Image;
+import java.util.Collections;
 
 public class Fox extends Animal {
     private static final int sightDistance = 10;
@@ -19,6 +21,46 @@ public class Fox extends Animal {
 
     public void act(Grid grid){
         Debug.echo("Here is where the Fox would act");
+        
+        GridSquare mySquare = grid.get(getLocation());
+        List<DistanceSquarePair> visibleSquares = grid.getAdjacentSquares(getLocation(), sightDistance);
+        List<DistanceSquarePair> predatorSquares = grid.getOrganismSquares(visibleSquares, predators);
+        List<DistanceSquarePair> preySquares = grid.getOrganismSquares(visibleSquares, prey);
+        List<DistanceSquarePair> emptySquares = grid.getEmptySquares(visibleSquares);
+        
+        Plant myPlant = mySquare.getPlant();
+        if(predatorSquares.size() > 0) {
+            Debug.echo("OH SHIT RUN?");
+        }
+        if (myPlant != null && myPlant.isAlive() && prey.contains(myPlant.getClass().getName())){
+            myPlant.getEaten(10);
+            eat(10);
+            return;
+        } else {
+            List<DistanceSquarePair> reachableSquares = grid.getAdjacentSquares(getLocation(), moveDistance);
+            Collections.shuffle(reachableSquares);
+            for(DistanceSquarePair pair: reachableSquares){
+                if(preySquares.contains(pair)){
+                    if(emptySquares.contains(pair)){
+                        move(grid, pair.gridSquare);
+                
+                        mySquare = grid.get(getLocation());
+                        myPlant = mySquare.getPlant();
+                        myPlant.getEaten(10);
+                        eat(10);
+                        return;
+                    } else {
+                        grid.removeAnimal(pair.gridSquare);
+                        move(grid, pair.gridSquare);
+                        eat(20);
+                        return;
+                    }
+                }
+            }
+            List<DistanceSquarePair> emptyReachableSquares = emptySquares;
+            emptyReachableSquares.retainAll(reachableSquares);
+            move(grid, emptyReachableSquares.get(Util.randInt(emptyReachableSquares.size())).gridSquare);
+        }
     }
 
     public Image getImage(){
