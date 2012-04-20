17fa932bbcf322c5d32dc280bb9d9c0a19fd3e96
Trevor Griswold <tgriswol@willamette.edu>
2012-04-19 21:45:33 -0700

    Work in progress

--- Fox.java
+++ Fox.java
@@ -8,7 +8,7 @@ public class Fox extends Animal {
 
     private static final int sightDistance = 10;
     private static final int moveDistance = 2;
-    private static final int maxHunger = 100;
+    private static int maxHunger;
     private static final int maxAge = 100;
     
     private static ArrayList<String> prey = new ArrayList<String>();
@@ -30,53 +30,69 @@ public class Fox extends Animal {
         List<DistanceSquarePair> preySquares = grid.getOrganismSquares(visibleSquares, prey);
         List<DistanceSquarePair> emptySquares = grid.getEmptySquares(visibleSquares);
         
-        Plant myPlant = mySquare.getPlant();
+        
+        
         if(predatorSquares.size() > 0) {
             Debug.echo("OH SHIT RUN?");
         }
-        if (myPlant != null && myPlant.isAlive() && prey.contains(myPlant.getClass().getName())){
-            myPlant.getEaten(10);
-            eat(10);
-            return;
-        } else {
-            List<DistanceSquarePair> reachableSquares = grid.getAdjacentSquares(getLocation(), moveDistance);
-            Collections.shuffle(reachableSquares);
-            for(DistanceSquarePair pair: reachableSquares){
-                if(preySquares.contains(pair)){
-                    if(emptySquares.contains(pair)){
-                        move(grid, pair.gridSquare);
-                
-                        mySquare = grid.get(getLocation());
-                        myPlant = mySquare.getPlant();
-                        myPlant.getEaten(10);
-                        eat(10);
-                        return;
-                    } else {
-                        grid.removeAnimal(pair.gridSquare);
-                        move(grid, pair.gridSquare);
-                        eat(20);
-                        return;
-                    }
+        
+        Plant myPlant = null;
+        if (mySquare.getPlant() != null && mySquare.getPlant().isAlive() && prey.contains(mySquare.getPlant().getClass().getName())){
+            myPlant = mySquare.getPlant();
+        }
+        GridSquare bestPreySquare = null;
+        List<DistanceSquarePair> reachableSquares = grid.getAdjacentSquares(getLocation(), moveDistance);
+        Collections.shuffle(reachableSquares);
+        for(DistanceSquarePair pair: reachableSquares){
+            if(preySquares.contains(pair)){
+                if(emptySquares.contains(pair)){
+                    move(grid, pair.gridSquare);
+            
+                    myPlant = grid.get(getLocation()).getPlant();
+                    eat(myPlant.getEaten());
+                    return;
+                } else {
+                    eat(pair.gridSquare.getAnimal().getCalories());
+                    grid.removeAnimal(pair.gridSquare);
+                    move(grid, pair.gridSquare);
+                    return;
                 }
             }
-            List<DistanceSquarePair> emptyReachableSquares = emptySquares;
-            emptyReachableSquares.retainAll(reachableSquares);
-            if (emptyReachableSquares.size() > 0) {
-                move(grid, emptyReachableSquares.get(Util.randInt(emptyReachableSquares.size())).gridSquare);
+        }
+        if(bestPreySquare != null) {
+            if(bestPreySquare.getAnimal() == null){
+                move(grid, bestPreySquare);
+                eat(bestPreySquare.getPlant().getEaten());
+            } else {
+                eat(bestPreySquare.getAnimal().getCalories());
+                grid.removeAnimal(bestPreySquare);
+                move(grid, bestPreySquare);
             }
+            return;
+        } else if (myPlant != null) {
+            eat(myPlant.getEaten());
+        }
+        
+        List<DistanceSquarePair> emptyReachableSquares = emptySquares;
+        emptyReachableSquares.retainAll(reachableSquares);
+        if (emptyReachableSquares.size() > 0) {
+            move(grid, emptyReachableSquares.get(Util.randInt(emptyReachableSquares.size())).gridSquare);
         }
     }
 
     public static void addPrey(String p)     { prey.add(p);      }
     public static void addPredator(String p) { predators.add(p); }
-    public static void setCalories(int c)    { calories = c;     }
-    public static int getCalories()          { return calories;  }
+    public static void setCalories(int c)    { 
+        calories = c;     
+        maxHunger = c * 10;
+    }
 
     protected int getMaxHunger()    { return maxHunger;                     }
     protected int getMaxAge()       { return maxAge;                        }
     protected int getSightDistance(){ return sightDistance;                 }
     protected int getMoveDistance() { return moveDistance;                  }
     
+    public int getCalories()        { return calories;                      }
     public String toString()        { return "Fox";                         }
     public Image getImage()         { return Resources.imageByName("Fox");  }
 }
